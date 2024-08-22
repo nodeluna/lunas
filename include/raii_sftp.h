92a04e1c2d86a0c23cd7ba3c69947c7b12950f23
nodeluna <nodeluna@proton.me>
2024-08-22 11:27:41 +0300

    added raii_sftp.cpp and raii_sftp.h files to better handle sftp/ssh objects

---
+++ include/raii_sftp.h
@@ -0,0 +1,80 @@
+#ifndef RAII_SFTP
+#define RAII_SFTP
+
+#include "config.h"
+
+#ifdef REMOTE_ENABLED
+#include <libssh/sftp.h>
+
+#include <string>
+
+
+#define REMOTE_BUFFER_SIZE 65536 * 2
+
+namespace raii{
+	namespace sftp{
+		class session{
+			sftp_session* _sftp;
+			public:
+				explicit session(sftp_session* sftp_);
+				~session();
+		};
+
+		class channel{
+			ssh_channel* _channel;
+			public:
+				explicit channel(ssh_channel* channel_);
+				~channel();
+		};
+
+		class attributes{
+			sftp_attributes* _attributes;
+			public:
+				explicit attributes(sftp_attributes* attr);
+				~attributes();
+		};
+
+		class dir{
+			sftp_dir* _dir;
+			std::string _path;
+			public:
+				explicit dir(sftp_dir* dir, const std::string& path);
+				~dir();
+		};
+
+		class file{
+			sftp_file* _file;
+			std::string _path;
+			public:
+				explicit file(sftp_file* file, const std::string& path);
+				~file();
+		};
+
+		class link_target{
+			char** target;
+			public:
+				explicit link_target(char** target);
+				~link_target();
+		};
+		class statvfs_t{
+			sftp_statvfs_t* _partition_stats;
+			public:
+				explicit statvfs_t(sftp_statvfs_t* parition_stats);
+				~statvfs_t();
+		};
+	}
+
+}
+
+namespace sftp{
+	int unlink(const sftp_session& sftp, const std::string& path);
+	int rmdir(const sftp_session& sftp, const std::string& path);
+	sftp_attributes attributes(const sftp_session& sftp, const std::string& path);
+	std::string cmd(const ssh_session& ssh, const std::string& command, const std::string& ip);
+	std::string homedir(const ssh_session& ssh, const std::string& ip);
+	std::string cwd(const ssh_session& ssh, const std::string& ip);
+}
+
+#endif // REMOTE_ENALBED
+
+#endif // RAII_SFTP
---
+++ src/raii_sftp.cpp
@@ -0,0 +1,158 @@
+#include "config.h"
+
+#ifdef REMOTE_ENABLED
+
+#include <iostream>
+#include <libssh/sftp.h>
+#include "raii_sftp.h"
+#include "log.h"
+
+namespace raii{
+	namespace sftp{
+		session::session(sftp_session* sftp_) : _sftp(sftp_){
+		}
+
+		session::~session(){
+			if(*_sftp == NULL)
+				return;
+			sftp_free(*_sftp);
+		}
+
+		channel::channel(ssh_channel* channel_) : _channel(channel_){
+		}
+
+		channel::~channel(){
+			if(*_channel == NULL)
+				return;
+			ssh_channel_close(*_channel);
+			ssh_channel_free(*_channel);
+		}
+
+
+		attributes::attributes(sftp_attributes* attr) : _attributes(attr){
+		}
+
+		attributes::~attributes(){
+			if(*_attributes == NULL)
+				return;
+			sftp_attributes_free(*_attributes);
+		}
+		
+		dir::dir(sftp_dir* dir, const std::string& path) : _dir(dir), _path(path){
+		}
+
+		dir::~dir(){
+			if(*_dir == NULL)
+				return;
+
+			int rc = sftp_closedir(*_dir);
+			if (rc == SSH_OK)
+				return;
+
+			llog::error("Couldn't close dir '" +  _path + "', " + ssh_get_error((*_dir)->sftp->session));
+		}
+
+		file::file(sftp_file* file, const std::string& path) : _file(file), _path(path){
+		}
+
+		file::~file(){
+			if(*_file == NULL)
+				return;
+
+			int rc = sftp_close(*_file);
+			if (rc == SSH_OK)
+				return;
+
+			llog::error("-[X] Couldn't close file '" +  _path + "', " + ssh_get_error((*_file)->sftp->session));
+		}
+
+		link_target::link_target(char** target_) : target(target_){
+		}
+
+		link_target::~link_target(){
+			if(target == NULL)
+				return;
+			ssh_string_free_char(*target);
+		}
+		statvfs_t::statvfs_t(sftp_statvfs_t* partition_stats) : _partition_stats(partition_stats) {
+
+		};
+		statvfs_t::~statvfs_t(){
+			if(*_partition_stats == NULL)
+				return;
+			sftp_statvfs_free(*_partition_stats);
+		}
+	}
+}
+
+namespace sftp{
+	int unlink(const sftp_session& sftp, const std::string& path){
+		if(options::dry_run == false)
+			return sftp_unlink(sftp, path.c_str());
+		return SSH_OK;
+	}
+	int rmdir(const sftp_session& sftp, const std::string& path){
+		if(options::dry_run == false)
+			return sftp_rmdir(sftp, path.c_str());
+		return SSH_OK;
+	}
+	sftp_attributes attributes(const sftp_session& sftp, const std::string& path){
+		sftp_attributes attributes;
+		if(options::follow_symlink)
+			attributes = sftp_stat(sftp, path.c_str());
+		else
+			attributes = sftp_lstat(sftp, path.c_str());
+		return attributes;
+	}
+
+	std::string cmd(const ssh_session& ssh, const std::string& command, const std::string& ip){
+		std::string output;
+		ssh_channel channel = ssh_channel_new(ssh);
+		int rc = ssh_channel_open_session(channel);
+		if(rc != SSH_OK){
+			llog::remote_error_ssh(ssh, ip, "couldn't excute '" + command + "'", NO_EXIT);
+			return "";
+		}
+		raii::sftp::channel channel_obj = raii::sftp::channel(&channel);
+
+		rc = ssh_channel_request_exec(channel, command.c_str());
+		if(rc != SSH_OK){
+			llog::remote_error_ssh(ssh, ip, "couldn't excute '" + command + "'", NO_EXIT);
+			return "";
+		}
+
+		char buffer[REMOTE_BUFFER_SIZE];
+		while(true){
+			int nbytes = ssh_channel_read(channel, buffer, sizeof(buffer), 0);
+			if(nbytes < 0){
+				llog::remote_error_ssh(ssh, ip, "error while getting '" + command + "' output", NO_EXIT);
+				return "";
+			}
+			if(nbytes == 0)
+				break;
+			output.append(buffer, nbytes);
+		}
+
+		if(output.empty() != true && output.back() == '\n')
+			output.pop_back();
+
+		return output;
+	}
+	
+	std::string homedir(const ssh_session& ssh, const std::string& ip){
+		std::string path = sftp::cmd(ssh, "echo $HOME", ip);
+		if(path.empty())
+			llog::error("couldn't get home directory for '" + ip + "' environment variable is empty");
+		os::append_seperator(path);
+		return path;
+	}
+
+	std::string cwd(const ssh_session& ssh, const std::string& ip){
+		std::string path = sftp::cmd(ssh, "pwd", ip);
+		if(path.empty())
+			llog::error("couldn't get cwd directory for '" + ip + "'");
+		os::append_seperator(path);
+		return path;
+	}
+}
+#endif // REMOTE_ENABLED
