59824cd1f3d5fd28d1999ae71f45279597f2c36e
nodeluna <nodeluna@proton.me>
2024-08-22 11:56:16 +0300

    added cppfs.cpp cppfs.h files to better wrap std::filesystem functions around the correct options

---
+++ include/cppfs.h
@@ -0,0 +1,15 @@
+#ifndef CPPFS
+#define CPPFS
+
+#include <filesystem>
+#include <string>
+
+namespace cppfs{
+	/*
+	std::error_code remove(const std::string& path);
+	std::filesystem::file_status status(const std::string& path, std::error_code& ec);
+	*/
+	std::filesystem::file_status copy(const std::string& src, const std::string& dest, std::error_code& ec);
+}
+
+#endif // CPPFS
---
+++ src/cppfs.cpp
@@ -0,0 +1,33 @@
+#include <filesystem>
+#include <string>
+#include "config.h"
+
+namespace cppfs{
+	/*std::error_code remove(const std::string& path){
+		std::error_code ec;
+		if(options::dry_run == false)
+			std::filesystem::remove(path, ec);
+		return ec;
+	}
+	std::filesystem::file_status status(const std::string& path, std::error_code& ec){
+		if(options::follow_symlink)
+			return std::filesystem::status(path, ec);
+		else
+			return std::filesystem::symlink_status(path, ec);
+	}*/
+	void copy(const std::string& src, const std::string& dest, std::error_code& ec){
+		std::filesystem::copy_options opts = std::filesystem::copy_options::none;
+
+		if(options::follow_symlink)
+			opts = opts | std::filesystem::copy_options::copy_symlinks;
+
+		if(options::update)
+			opts = opts | std::filesystem::copy_options::update_existing;
+		else if(options::rollback)
+			opts = opts | std::filesystem::copy_options::overwrite_existing;
+
+		if(options::dry_run)
+			std::filesystem::copy(src, dest, opts, ec);
+
+	}
+}
