22ba12a655c34750bc0945f1bebf385b0dc421f4
nodeluna <nodeluna@proton.me>
2024-11-15 20:34:06 +0200

    updated nix flake to latest version

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
                 let
                 pkgs = import nixpkgs { inherit system; };
                 pname = "lunas";
-                version = "2.0.8";
+                version = "2.1.1";
                 in
                 {
 
